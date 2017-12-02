23754f28b6107d8c8be57d9d20e76309b3e3fe1e
Mohanson <mohanson@outlook.com>
2017-12-02 22:36:03 +0800

    2017-12-02 22:36:03

--- make.py
+++ make.py
@@ -1,5 +1,4 @@
 import glob
-import os.path
 import re
 import subprocess
 
@@ -10,7 +9,9 @@ def main():
 
     pattern = re.compile(r'((?<=href=")|((?<=src=")))(?P<src>.*?\.((jpg)|(png)|(gif)|(svg)|(bmp)|(css)|(js)))(?=")')
 
-    def repl(x): return 'http://cdn.accu.cc/{0}'.format(x.group('src').strip('./'))
+    def repl(x):
+        return 'http://cdn.accu.cc/{0}'.format(x.group('src').strip('./'))
+
     for entry in glob.glob('**/*.html', recursive=True):
         print(entry)
         with open(entry, 'r+', encoding='utf-8') as f:
@@ -23,9 +24,6 @@ def main():
         f.write('Pem1L7uAVI')
     with open('./site/google9b75b4b4147e247b.html', 'w') as f:
         f.write('google-site-verification: google9b75b4b4147e247b.html')
-
-    if not os.path.exists('site/page'):
-        subprocess.call('ln -s -f content page', shell=True, cwd='site')
 
 
 if __name__ == '__main__':
